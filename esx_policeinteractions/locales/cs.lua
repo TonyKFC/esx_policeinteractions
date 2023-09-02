a609dfd93d4658ed984f450b13ab3fcc4fbc4a8f
miskaslzickou <ilikemoonm@gmail.com>
2023-09-02 14:07:05 +0200

    added cs locale

---
+++ esx_policeinteractions/locales/cs.lua
@@ -0,0 +1,12 @@
+Locales['cs'] = {
+    ['HandCuff_uncuff'] = 'Spoutat ruce / odpoutat',
+    ['FeetCuff_uncuff'] = 'Spoutat nohy / odpoutat',
+    ['Darg'] = 'Vzít',
+    ['Sechce'] = 'Prohledat',
+    ['PutInVehiclece'] = 'Dát do vozidla',  
+    ['OutVehiclece'] = 'Vzít z vozidla',
+    ['StopDargging'] = '[G] - Přestat brát',
+    ['searching'] = 'Prohledávám',
+  
+  
+  }
